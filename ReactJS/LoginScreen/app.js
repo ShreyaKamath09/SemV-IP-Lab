bdee4345246355d99c8b8153ff234a9a7acd6420
Shreya Kamath <113795442+ShreyaKamath09@users.noreply.github.com>
2023-08-31 23:33:24 +0530

    Create app.js

---
+++ ReactJS/LoginScreen/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; 
+import LoginScreen from './LoginScreen'; 
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Login App</h1>
+      <LoginScreen />
+    </div>
+  );
+}
+
+export default App;
