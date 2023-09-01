83f37c1521ea845e5480086f8ee9a45ec327c66b
Shreya Kamath <113795442+ShreyaKamath09@users.noreply.github.com>
2023-09-01 11:54:17 +0530

    Add files via upload

---
+++ ReactJS/Router/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+  
+function About (){
+    return(
+        <>
+        <h1>This is the About Page!</h1>
+        <h2>Welcome!</h2>
+        <h3>I am a third year IT engineering student.</h3>
+        </>
+    )
+}
+  
+export default About;
---
+++ ReactJS/Router/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+  
+function Home (){
+    return(
+        <>
+        <h1>This is the Home Page!</h1>
+        <h2>Welcome!</h2>
+        <h3>I am Shreya Kamath!</h3>
+        </>
+    )
+}
+  
+export default Home;
